4a1f7e4e26178f3ef7659aae1167d33bcd296c67
Thang Luong <lhthang96@gmail.com>
2022-04-26 22:39:37 +0700

    chore: removed redundant dependencies

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -16,11 +16,6 @@ const config = {
     fallback: {
       crypto: require.resolve('crypto-browserify'),
       stream: require.resolve('stream-browserify'),
-      assert: require.resolve('assert'),
-      http: require.resolve('stream-http'),
-      https: require.resolve('https-browserify'),
-      os: require.resolve('os-browserify'),
-      url: require.resolve('url'),
     },
   },
   module: {
